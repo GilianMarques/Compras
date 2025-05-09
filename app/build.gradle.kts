415b1574dcee10672019709b0b8747f856d00472
Gilian Marques Fernandes <gilian762@gmail.com>
2025-05-09 20:27:39 -0300

    gradle atualizado e libs atualizadas
    - gradle 8.11.1
    - Android Gradle plugin 8.10.0
    - Kotlin 2.1.20
    - lifecycle-livedata,viewmodel,runtime-ktx 2.9.0
    - firebase-firestore 25.1.4
    - firebase-config 22.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.8.2"
-    id("org.jetbrains.kotlin.android") version "2.1.0"
+    id("com.android.application") version "8.10.0"
+    id("org.jetbrains.kotlin.android") version "2.1.20"
     id("com.google.gms.google-services") version "4.4.2"
     id("kotlin-kapt")
 }
@@ -56,25 +56,25 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
     implementation("com.google.android.gms:play-services-auth:21.3.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("com.ericktijerou.koleton:koleton:1.0.0-beta01")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("com.google.firebase:firebase-auth:23.2.0")
     implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
-    implementation("com.google.firebase:firebase-firestore:25.1.3")
+    implementation("com.google.firebase:firebase-firestore:25.1.4")
     implementation("io.github.androidpoet:dropdown:1.1.6")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
-    implementation("com.google.firebase:firebase-config:22.1.0")
+    implementation("com.google.firebase:firebase-config:22.1.1")
     implementation("com.google.firebase:firebase-core:21.1.1")
     implementation("com.firebaseui:firebase-ui-auth:9.0.0")
 
